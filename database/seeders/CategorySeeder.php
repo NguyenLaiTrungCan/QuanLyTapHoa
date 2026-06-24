7cc14c1a95a71d27fdb41bb83c3fa9fdf5b133e3
Phuc <shirosama1239@gmail.com>
2026-06-24 22:21:54 +0700

    merge tam de pull cai khac

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,11 +10,31 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Đồ uống', 'description' => 'Nước ngọt, nước suối, trà, cà phê và các loại đồ uống khác.'],
-            ['name' => 'Bánh kẹo', 'description' => 'Các loại bánh, kẹo và snack.'],
-            ['name' => 'Gia vị', 'description' => 'Nước mắm, muối, đường, tiêu và gia vị nấu ăn.'],
-            ['name' => 'Mì gói', 'description' => 'Mì ăn liền, phở ăn liền, cháo ăn liền.'],
-            ['name' => 'Sữa & thực phẩm khô', 'description' => 'Sữa, ngũ cốc, đồ khô và thực phẩm đóng gói.'],
+           [
+                'id' => 1,
+                'name' => 'Đồ uống / Giải khát',
+                'description' => 'Các loại nước ngọt có ga, nước tinh khiết, trà, cà phê.',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Bánh kẹo / Ăn vặt',
+                'description' => 'Các loại bánh ngọt, bánh quy, kẹo, và đồ ăn vặt các loại.',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Thực phẩm khô / Gia vị',
+                'description' => 'Các loại gia vị nấu ăn, đồ khô dự trữ.', // Thêm vào để lấp khoảng trống ID 3
+            ],
+            [
+                'id' => 4,
+                'name' => 'Đồ ăn liền',
+                'description' => 'Các loại mì tôm, phở, miến gói, đồ hộp tiện lợi.',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Sữa / Sản phẩm từ sữa',
+                'description' => 'Sữa tươi, sữa chua, sữa hạt và các chế phẩm khác từ sữa.',
+            ],
         ];
 
         foreach ($categories as $category) {
