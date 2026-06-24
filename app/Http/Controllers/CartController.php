a60b2b5e5c5fbd200c6bf9d2ba2e494b1b7cfca2
Phuc <shirosama1239@gmail.com>
2026-06-25 00:36:14 +0700

    fix lỗi

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,6 +6,7 @@ use App\Models\Cart;
 use App\Models\Product;
 use App\Models\Inventory;
 use App\Http\Requests\CartRequest;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
